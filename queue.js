15a4028baedfb81fb0beaa8374f290553c7ba6bd
Fabiana Balduína <fabiana.silva1@estudante.ifb.edu.br>
2024-07-13 12:31:52 -0300

    Entrega desafio PretaLab

--- queue.js
+++ queue.js
@@ -1,10 +1,27 @@
 class Queue {
-  elements
-  constructor(){
-    this.elements = []
+  constructor() {
+    this.elements = [];
   }
-  
-  // Implementar aqui
+
+  // Método para calcular o tamanho da fila
+  size() {
+    return this.elements.length;
+  }
+
+  // Método para adicionar um item na fila
+  add(item) {
+    this.elements.push(item);
+  }
+
+  // Método para escolher o primeiro item da fila (sem remover)
+  peek() {
+    return this.elements[0];
+  }
+
+  // Método para remover e retornar o primeiro item da fila
+  dequeue() {
+    return this.elements.shift();
+  }
 }
 
-module.exports = Queue
+module.exports = Queue;
--- queue.test.js
+++ queue.test.js
@@ -1,25 +1,38 @@
-const Queue = require('./queue')
+const Queue = require('./queue');
 
 describe('Queue', () => {
-  it('Deve criar uma fila', () => {
-    const queue = new Queue()
-
-    expect(queue).toEqual({ elements: []})
-  })
+  it('Deve criar uma fila vazia', () => {
+    const queue = new Queue();
+    expect(queue.elements).toEqual([]);
+  });
 
   it('Deve ser capaz de calcular o tamanho da fila', () => {
-    // Seu código aqui
-  })
+    const queue = new Queue();
+    queue.add('item1');
+    queue.add('item2');
+    expect(queue.size()).toBe(2);
+  });
 
   it('Deve ser capaz de adicionar um item na fila', () => {
-   // Seu código aqui
-  })
+    const queue = new Queue();
+    queue.add('item1');
+    expect(queue.elements).toEqual(['item1']);
+  });
 
   it('Deve escolher o primeiro item da fila', () => {
-    // Seu código aqui
-  })
+    const queue = new Queue();
+    queue.add('item1');
+    queue.add('item2');
+    expect(queue.peek()).toBe('item1');
+  });
 
   it('Deve remover o primeiro item da fila', () => {
-    // Seu código aqui
-  })
-})
+    const queue = new Queue();
+    queue.add('item1');
+    queue.add('item2');
+    const removedItem = queue.dequeue();
+    expect(removedItem).toBe('item1');
+    expect(queue.size()).toBe(1);
+    expect(queue.peek()).toBe('item2');
+  });
+});
